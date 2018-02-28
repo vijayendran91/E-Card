4592f48c7bc20ae67e86c9c5ed20d5cef9c3de91
Vijayendran <Vijayendran91@gmail.com>
2018-02-28 15:27:45 +0530

    Heroku edit 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   resources :user
   root :to => "welcome#index"
-  get '/profile', to: 'user#profile'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
